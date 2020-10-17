2459ac929e10d16ae1d7de1899ef0cfe190687cf
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-10-17 10:58:28 +0200

    Better encapsulate unsafe

--- src/gsl.rs
+++ src/gsl.rs
@@ -16,18 +16,22 @@ pub(crate) fn lock() -> Guard {
         return Guard { guard: None };
     }
 
-    let guard = unsafe {
-        MUTEX_INIT.call_once(|| ptr::write(MUTEX.as_mut_ptr(), Mutex::new(())));
-        (*MUTEX.as_ptr()).lock().unwrap()
-    };
+    let guard = static_mutex().lock().unwrap_or_else(|err| err.into_inner());
     LOCKED.with(|it| it.set(true));
     Guard { guard: Some(guard) }
 }
 
-static mut MUTEX: MaybeUninit<Mutex<()>> = MaybeUninit::uninit();
-static MUTEX_INIT: Once = Once::new();
+fn static_mutex() -> &'static Mutex<()> {
+    static mut MUTEX: MaybeUninit<Mutex<()>> = MaybeUninit::uninit();
+    static MUTEX_INIT: Once = Once::new();
+    unsafe {
+        MUTEX_INIT.call_once(|| ptr::write(MUTEX.as_mut_ptr(), Mutex::new(())));
+        &*MUTEX.as_ptr()
+    }
+}
+
 thread_local! {
-    pub static LOCKED: Cell<bool> = Cell::new(false);
+    static LOCKED: Cell<bool> = Cell::new(false);
 }
 
 impl Drop for Guard {
